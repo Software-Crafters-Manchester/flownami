adef6064d78cf39e69a2dfd95ea60504311bc1bb
Joe Mosley <joemosley96@gmail.com>
2025-03-13 19:22:02 +0000

    Implements Add Task via a POST request

--- main.ts
+++ main.ts
@@ -3,9 +3,16 @@ import express from "npm:express";
 
 const app = express();
 
+const filePath = "./database.json";
+
+const jsonText = await Deno.readTextFile(filePath);
+const data = JSON.parse(jsonText);
+
 app.set("view engine", "ejs");
 
 app.use(express.static("static"));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (_, res) {
   res.render("pages/index");
@@ -21,53 +28,21 @@ type Task = {
 };
 
 app.get("/board", function (_req, res) {
-  const columns: Array<Column> = [
-    {
-      name: "To Do",
-      tasks: [
-        {
-          name: "Task 1",
-        },
-        {
-          name: "Task 2",
-        },
-        {
-          name: "Task 3",
-        },
-      ],
-    },
-    {
-      name: "Doing",
-      tasks: [
-        {
-          name: "In progress...",
-        },
-      ],
-    },
-    {
-      name: "Done",
-      tasks: [
-        {
-          name: "Finished this one",
-        },
-        {
-          name: "And this one",
-        },
-        {
-          name: "Crikey,",
-        },
-        {
-          name: "Wasn't I...",
-        },
-        {
-          name: "Productive!",
-        },
-      ],
-    },
-  ];
-
+  const columns: Array<Column> = data
   res.render("pages/board", { columns });
 });
+
+app.get("/create", function (_req, res){
+  res.render("pages/create")
+})
+
+app.post("/create", function(_req, res){
+  const toDo = data.find((column: Column) => column.name === "To Do")
+  const newTask = _req.body.task
+  toDo.tasks.push({name: newTask})
+  Deno.writeTextFile(filePath, JSON.stringify(data))
+  res.redirect("/board")
+})
 
 const port = Deno.env.get("PORT") || 8080;
 app.listen(port);
