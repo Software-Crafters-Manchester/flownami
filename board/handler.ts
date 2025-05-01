30cb9a713211eb464591d8989525eff90c919bec
alasdairsmith <alasdairsmith@>
2025-05-01 17:49:58 +0100

    Add test for generateBoard

--- board/handler.ts
+++ board/handler.ts
@@ -2,11 +2,12 @@
 import express from "npm:express";
 
 import { generateBoard } from "./service.ts";
+import { JSONTaskRepo } from "../data.ts";
 
 const boardRouter = express();
 
 boardRouter.get("/", async function (_req, res) {
-  const board = await generateBoard();
+  const board = await generateBoard(JSONTaskRepo);
 
   res.render("./board/pages", { board });
 });
--- board/service.ts
+++ board/service.ts
@@ -1,13 +1,15 @@
-import { readTasks } from "../data.ts";
+import { TaskRepo } from "../data.ts";
 import { Task } from "../tasks/Task.ts";
 import { Board, Column } from "./Board.ts";
 
-export async function generateBoard() {
-  const tasks = await readTasks();
+export async function generateBoard(taskRepo: TaskRepo) {
+  const tasks = await taskRepo.readTasks();
+  // const tasks = await taskRepo.readTasks(JSONTaskRepo);
 
   const board = buildBoard(tasks);
   return board;
 }
+
 function buildBoard(tasks: Task[]): Board {
   const todoTasks: Task[] = tasks.filter((t) => t.column === "To Do");
   const doingTasks: Task[] = tasks.filter((t) => t.column === "Doing");
